53f66be4ef3d7575d519bb7412f8c520345d8f3a
Judy1692001 <74277086+Judy1692001@users.noreply.github.com>
2023-12-31 10:31:17 +0200

    final changes

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -20,60 +20,60 @@ public class OrderController {
     }
 
     @PostMapping("/createsimpleorder")
-    public Order PlaceSimpleOrder(@RequestBody Customer c, @RequestBody Order simpleOrder)
+    public Order PlaceSimpleOrder( Customer c,  Order simpleOrder)
     {
         return obl.createSimpleOrder(c, simpleOrder);
     }
 
-//    @PostMapping("/createcompoundorder")
-//    public List<Order> PlaceCompoundOrder(@RequestBody List <Customer> customers, @RequestBody List<Order> compoundOrder)
-//    {
-//        return obl.createCompoundOrder(customers, compoundOrder);
-//    }
-//
-//    @PutMapping("/shipsimpleorder")
-//    public String ShipSimpleOrder(@RequestBody Customer c, @RequestBody Order simpleOrder)
-//    {
-//        obl.shipSimpleOrder(c, simpleOrder);
-//
-//        return "Order Shipped Successfully!";
-//    }
-//
-//    @PutMapping("/shipcompoundorder")
-//    public String ShipCompoundOrder(@RequestBody List <Customer> c, @RequestBody List <Order> compoundOrder)
-//    {
-//        obl.shipCompoundOrder(c, compoundOrder);
-//
-//        return "Order Shipped Successfully!";
-//    }
-//    @PutMapping("/cancelsimpleorderShipping")
-//    public String CancelSimpleOrderShipping(@RequestBody Customer c, @RequestBody Order simpleOrder)
-//    {
-//        obl.cancelSimpleOrderShipping(c, simpleOrder);
-//
-//        return "Shipping canceled Successfully!";
-//    }
-//
-//    @PutMapping("/cancelcompoundorderShipping")
-//    public String CancelCompoundOrderShipping(@RequestBody List <Customer> c, @RequestBody List <Order> compoundOrder)
-//    {
-//        obl.cancelCompoundOrderShipping(c, compoundOrder);
-//
-//        return "Shipping canceled Successfully!";
-//    }
-//    @PutMapping("/cancelsimpleorder")
-//    public String CancelSimpleOrder(@RequestBody Customer c, @RequestBody Order simpleOrder)
-//    {
-//        obl.cancelallSimpleOrder(c, simpleOrder);
-//
-//        return "Order canceled Successfully!";
-//    }
-//
-//    @PutMapping("/cancelcompoundorder")
-//    public String CancelCompoundOrder(@RequestBody List <Customer> c, @RequestBody List <Order> compoundOrder)
-//    {
-//        obl.cancelallCompoundOrder(c, compoundOrder);
-//
-//        return "Order canceled Successfully!";
-//    }
+    @PostMapping("/createcompoundorder")
+    public List<Order> PlaceCompoundOrder(@RequestBody List <Customer> customers, @RequestBody List<Order> compoundOrder)
+    {
+        return obl.createCompoundOrder(customers, compoundOrder);
+    }
+
+    @PutMapping("/shipsimpleorder")
+    public String ShipSimpleOrder(@RequestBody Customer c, @RequestBody Order simpleOrder)
+    {
+        obl.shipSimpleOrder(c, simpleOrder);
+
+        return "Order Shipped Successfully!";
+    }
+
+    @PutMapping("/shipcompoundorder")
+    public String ShipCompoundOrder(@RequestBody List <Customer> c, @RequestBody List <Order> compoundOrder)
+    {
+        obl.shipCompoundOrder(c, compoundOrder);
+
+        return "Order Shipped Successfully!";
+    }
+    @PutMapping("/cancelsimpleorderShipping")
+    public String CancelSimpleOrderShipping(@RequestBody Customer c, @RequestBody Order simpleOrder)
+    {
+        obl.cancelSimpleOrderShipping(c, simpleOrder);
+
+        return "Shipping canceled Successfully!";
+    }
+
+    @PutMapping("/cancelcompoundorderShipping")
+    public String CancelCompoundOrderShipping(@RequestBody List <Customer> c, @RequestBody List <Order> compoundOrder)
+    {
+        obl.cancelCompoundOrderShipping(c, compoundOrder);
+
+        return "Shipping canceled Successfully!";
+    }
+    @PutMapping("/cancelsimpleorder")
+    public String CancelSimpleOrder(@RequestBody Customer c, @RequestBody Order simpleOrder)
+    {
+        obl.cancelallSimpleOrder(c, simpleOrder);
+
+        return "Order canceled Successfully!";
+    }
+
+    @PutMapping("/cancelcompoundorder")
+    public String CancelCompoundOrder(@RequestBody List <Customer> c, @RequestBody List <Order> compoundOrder)
+    {
+        obl.cancelallCompoundOrder(c, compoundOrder);
+
+        return "Order canceled Successfully!";
+    }
 }
